cd90771c93b00f1ad82833b5c51f0c92b4583923
Mahantesh Gattina <67777625+Mahantesh1729@users.noreply.github.com>
2022-01-08 21:26:55 +0530

    Add files via upload

---
+++ bmi_calculator/lib/constants.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+const KBottomContainerHeight = 80.0;
+const kActiveCardColor = Color(0xff1d1e33);
+const kInactiveCardColor = Color(0xff111328);
+const kBottomContainerColor = Color(0xffeb1555);
+
+const kLabelTextStyle = TextStyle(fontSize: 18.0, color: Color(0xff8d8e98));
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle =
+    TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold);
+
+const kTitleTextStyle = TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0XFF00FF00),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(fontSize: 100.0, fontWeight: FontWeight.bold);
+
+const kBodyTextStyle = TextStyle(fontSize: 22.0);
